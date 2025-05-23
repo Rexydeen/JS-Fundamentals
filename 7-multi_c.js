a9810a2dbeaa157cfdb78459e3f147cfcccc3c3f
Usman Shittu <usmanola1995@gmail.com>
2025-05-23 10:34:23 +0100

    corrected an error

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,4 +1,4 @@
 const dell = Number(process.argv[2]);
 if (isNaN(dell) || process.argv.length<3) {console.log("Missing number of occurrences");
-}	else if (dell > 0) {let i=0; while(i<dell) {console.log("C is fun"); i++;}
+}	else if (dell > 0) {let i=0; while (i<dell) {console.log("C is fun"); i++;}
 }
